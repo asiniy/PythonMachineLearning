bd7a6bd4932038ce4878a4560b831d4c6dc688a6
Alex Antonov <kaburbundokel11g@inbox.ru>
2017-12-30 09:48:30 +0300

    3 svm

--- 3/logistic_regression_gd.py
+++ 3/logistic_regression_gd.py
@@ -2,24 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from matplotlib.colors import ListedColormap
 
-def plot_decision_regions(X, y, classifier, resolution = 0.02):
-    # dot1_y = -(classifier.w_[0] + classifier.w_[1] * 1.) / classifier.w_[2]
-    # dot2_y = -(classifier.w_[0] + classifier.w_[1] * 5.) / classifier.w_[2]
+# def plot_decision_regions(X, y, classifier, resolution = 0.02):
+#     # dot1_y = -(classifier.w_[0] + classifier.w_[1] * 1.) / classifier.w_[2]
+#     # dot2_y = -(classifier.w_[0] + classifier.w_[1] * 5.) / classifier.w_[2]
+#
+#     # newline([1, dot1_y], [5, dot2_y])
+#
+#     markers = ('s', 'x', 'o', '^', 'v')
+#     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+#
+#     for idx, cl in enumerate(np.unique(y)):
+#         plt.scatter(x=X[y == cl, 0],
+#                     y=X[y == cl, 1],
+#                     alpha=0.8,
+#                     c=colors[idx],
+#                     marker=markers[idx],
+#                     label=cl,
+#                     edgecolor='black')
+#
+#     plt.xlabel('sepal length [cm]')
+#     plt.ylabel('petal length [cm]')
+#     plt.legend(loc='upper left')
+#     plt.show()
 
-    # newline([1, dot1_y], [5, dot2_y])
-
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
 
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0],
-                    y=X[y == cl, 1],
-                    alpha=0.8,
-                    c=colors[idx],
-                    marker=markers[idx],
-                    label=cl,
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl,
                     edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1],
+                    c='', edgecolor='black', alpha=1.0,
+                    linewidth=1, marker='o',
+                    s=100, label='test set')
 
 class LogisticRegressionGD(object):
     def __init__(self, eta=0.05, n_iter=100, random_state=1):
@@ -39,7 +77,7 @@ class LogisticRegressionGD(object):
             self.w_[1:] = self.eta * X.T.dot(errors)
             self.w_[0]  = self.eta * errors.sum()
 
-            cost = (-y.dot(np.log(output))) - ((1 - y).dot(np.log(1 - output)))
+            cost = (-y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))))
             self.cost_.append(cost)
 
         return self
@@ -48,7 +86,7 @@ class LogisticRegressionGD(object):
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def activation(self, Z):
-        return 1 / (1. + np.exp(-np.clip(z, -250, 250)))
+        return 1 / (1. + np.exp(-np.clip(Z, -250, 250)))
 
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, 0)
@@ -59,12 +97,29 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
-X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
-y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+# lrgd = LogisticRegressionGD(eta=0.05,
+#                             n_iter=1000,
+#                             random_state=1)
 
-lrgd = LogisticRegressionGD(eta=0.05,
-                            n_iter=1000,
-                            random_state=1)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
 
-lrgd.fit(X_train_01_subset, y_train_01_subset)
-plot_decision_regions(X=X_train_01_subset, y=y_train_01_subset, classifier=lrgd)
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+lr = LogisticRegression(C=100.0, random_state=1)
+lr.fit(X_train_std, y_train)
+# plot_decision_regions(X=X_combined_std, y=y_combined, classifier=lr)
+# plt.xlabel('petal length [standardized]')
+# plt.ylabel('petal width [standardized]')
+# plt.legend(loc='upper left')
+# plt.show()
+
+first_row = lr.predict_proba(X_test_std[:1, :])
+
+first = first_row.item(0)
+last = first_row.item(2)
+
+print(first > last)
