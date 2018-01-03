3d0d17b440d87c0ade2346628bfeb597dacb64a7
Alex Antonov <kaburbundokel11g@inbox.ru>
2018-01-03 17:05:26 +0300

    One-hot encoding

--- 4/nominal-ordinal.py
+++ 4/nominal-ordinal.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 df = pd.DataFrame([
     ['green', 'M', 10.1, 'class1'],
@@ -17,9 +17,19 @@ size_mapping = {
 
 df['size'] = df['size'].map(size_mapping)
 
+# X = df[['color', 'size', 'price']].values
+
+# color_le = LabelEncoder()
+# X[:, 0] = color_le.fit_transform(X[:, 0])
+# ohe = OneHotEncoder(categorical_features=[0], sparse=False)
+# print(ohe.fit_transform(X))
+
+
 class_le = LabelEncoder()
 y = class_le.fit_transform(df['classlabel'].values)
+df_y = pd.DataFrame({ 'class': y })
 
-df['classlabel'] = y
+dummy_features = pd.get_dummies(df[['price', 'color', 'size']])
 
+df = pd.concat([dummy_features, df_y], axis=1)
 print(df)
